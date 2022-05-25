e8c552f9873c990131469bb039cae88b371af7f0
Michael Mozaikov <45514035+MishkaBoss@users.noreply.github.com>
2022-05-25 20:15:08 +0300

    1st-day-submission

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 'use strict'
+const EMPTY = ''
 const MINE = '💣'
 const FLAG = '❗️'
 
@@ -15,42 +16,50 @@ var gGame = {
 function initGame() {
     gBoard = buildBoard()
     console.log(gBoard);
+    console.log(setMinesNegsCount(gBoard, 1, 2))
     renderBoard(gBoard)
+
 }
 
 
-// console.log(gBoard);
-function buildBoard() {
+
+function buildBoard() { //create baord 4x4
     var board = [];
     for (var i = 0; i < 4; i++) {
         board.push([])
         for (var j = 0; j < 4; j++) {
             board[i][j] = {
-                minesAroundCount: 4,
-                isShown: true,
+                minesAroundCount: 0,
+                isShown: false,
                 isMine: false,
-                isMarked: true
+                isMarked: false
             }
         }
     }
     // board[getRandomInt(0, 4)][getRandomInt(0, 4)].isMine = board[getRandomInt(0, 4)][getRandomInt(0, 4)].isMine = true //place 2 mines randomly
-    board[1][1].isMine = board[2][2].isMine = true //place 2 mines manually
 
+    board[1][1].isMine = board[2][2].isMine = true //place 2 mines manually
+    board[1][1].isShown = board[2][2].isShown = true
     return board;
 }
 
 // console.log(renderBoard(gBoard));
+function countNeighbors() {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            if (gBoard[i][j].isMine) gBoard[i][j].minesAroundCount = setMinesNegsCount(gBoard, i, j)
+            console.log(gBoard[i][j].minesAroundCount);
+        }
+    }
+}
 function renderBoard(board) {
     var strHTML = ''
     for (var i = 0; i < board.length; i++) {
         strHTML += `<tr>`
         for (var j = 0; j < board[0].length; j++) {
-            if (board[i][j].isMine) {
-                var cell = MINE
-            } else {
-                cell = ''
-            }
-            strHTML += `<td data-i="${i}" data-j="${j}" onclick="cellClicked(this, ${i}, ${j})" class="cell cell-${i}-${j}"> <button class="cell-btn">${cell}</button> </td>`
+            var cell = (board[i][j].isMine) ? MINE : board[i][j].minesAroundCount
+            var cellClass = `-${i}-${j}`
+            strHTML += `<td onclick="cellClicked(this, ${i}, ${j})" class="cell cell${cellClass}"> ${cell}</td>`
         }
         strHTML += `</tr>`
     }
@@ -58,8 +67,17 @@ function renderBoard(board) {
     elBoard.innerHTML = strHTML
 }
 
-function setMinesNegsCount(board) {
-
+function setMinesNegsCount(board, cellI, cellJ) {
+    var counter = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= board[0].length) continue;
+            if (i === cellI && j === cellJ) continue;
+            if (board[i][j].isMine) counter++
+        }
+    }
+    return counter
 }
 
 
@@ -68,14 +86,14 @@ function cellClicked(elCell, i, j) {
     console.log(`cell clicked!`);
 }
 
-// function cellMarked(elCell){
+function cellMarked(elCell) {
 
-// }
+}
 
-// function checkGameOver() {
+function checkGameOver() {
 
-// }
+}
 
-// function expandShown(board, elCell, i, j){
+function expandShown(board, elCell, i, j) {
 
-// }
+}
